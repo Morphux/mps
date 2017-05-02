74b2de656a4808b985e6b991230f9a0930c4fb13
Ne02ptzero <louis@ne02ptzero.me>
2017-05-02 14:58:56 +0200

    Fix(Tests): Update submodule, and fix test script

--- launch_test.sh
+++ launch_test.sh
@@ -3,13 +3,15 @@
 DB="mock3.db"
 IP="127.0.0.1"
 PORT="6666"
+P_CERT="tls/server.crt"
+PRIV_CERT="tls/server.key"
 
 
-./mps -db=./test_database/${DB} ${IP}:${PORT} &
+./mps -db=./test_database/${DB} -pub ${P_CERT} -priv ${PRIV_CERT} ${IP}:${PORT} &
 MPS_PID=$!
 sleep 2;
 echo ${MPS_PID}
-./tests/protocol_tests -a ${IP} -p ${PORT} -d test_database/${DB} client
+./tests/protocol_tests -a ${IP} -p ${PORT} -d test_database/${DB} -c ${P_CERT} client
 RET=$?
 kill ${MPS_PID}
 exit ${RET};
